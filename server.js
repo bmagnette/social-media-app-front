d3bc92d9956ad30447f285ab50f7dbe9c57dff2b
bmagnette <baptiste.magnette@gmail.com>
2022-01-19 18:27:08 +0100

    Changer server and add requirement

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const path = require('path');
-
+const favicon = require('express-favicon');
+const expressStaticGzip = require('express-static-gzip');
 const express = require('express');
 
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use("/", expressStaticGzip(path.join(__dirname, 'build')));
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.set('port', process.env.PORT || 8080);
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-var server = app.listen(app.get('port'), function () {
+var server = app.listen(port, function () {
     console.log('listening on port ', server.address().port);
 });
