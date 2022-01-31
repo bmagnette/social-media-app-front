b0dc02f7529f13da8b1e25300c77613505ba1945
bmagnette <baptiste.magnette@gmail.com>
2022-01-31 10:23:40 +0100

    add good id

--- src/auth/login/Login.tsx
+++ src/auth/login/Login.tsx
@@ -11,19 +11,7 @@ export const Login = () => {
     const [passwordError, setPasswordError] = useState('');
     const navigate = useNavigate();
     const auth = useAuth();
-    useEffect(() => {
-        window.$crisp = [];
-        window.CRISP_WEBSITE_ID = 'e38ca6ad-fa2b-434f-bb07-cb43120dc08c';
 
-        (function () {
-            const d = document;
-            const s = d.createElement('script');
-            s.src = 'https://client.crisp.chat/l.js';
-            s.async = Boolean(1);
-            s.id = 'chatID';
-            d.getElementsByTagName('head')[0].appendChild(s);
-        })();
-    }, []);
     return (
         <div className={'login-wrapper'}>
             <div id="back">
